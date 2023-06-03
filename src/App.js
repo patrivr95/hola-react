a5572b85e2d3ab0b73c44617247052292f8c2eb2
patrivr95 <patriciavadilloruiz@gmail.com>
2023-06-03 15:48:49 +0200

    componentes inicio

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
+import Greeting from './components/pure/greeting.jsx';
+import GreetingF from './components/pure/greetingF';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hola React
-        </p>
+        {/*Componente propio Greeting.jsx */}
+        {/* <Greeting name="Patricia"></Greeting> */}
+        <GreetingF name="Patricia"></GreetingF>
       </header>
     </div>
   );
