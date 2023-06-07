f099288a98ea9201a91ee731d6fcbdbcfe83a75f
patrivr95 <patriciavadilloruiz@gmail.com>
2023-06-07 20:17:09 +0200

    ejemplo de uso de hooks

--- src/components/container/task_list.jsx
+++ src/components/container/task_list.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect} from 'react';
 import { Task } from '../../models/task.class';
 import { LEVELS } from '../../models/levels.enum';
 import TaskComponent from '../pure/task';
@@ -7,11 +7,23 @@ const TaskListComponent = () => {
 
     const defaultTask = new Task('Example', 'Default description', false, LEVELS.NORMAL);
 
+    // estado del componente
+    const [task, setTasks] = useState([defaultTask]);
 
-    /* const changeState = (id) => {
-        console.log('ToDO: Cambiar estado de una tarea')
+
+    // Control del ciclo de vida del componente
+    useEffect(() => {
+        console.log("Task State has been modified");
+        return () => {
+            console.log("TaskList component is going to unmount");
+        };
+    }, [tasks]);
+    
+
+    const changeCompleted = (id) => {
+        console.log('TODO: Cambiar estado de una tarea')
     }
- */
+ 
 
     return (
         <div>
